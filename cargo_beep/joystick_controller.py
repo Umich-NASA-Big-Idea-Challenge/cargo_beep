65ede023362cf728bb357e2ce614ab3a87ee9933
Luke Weaver <ljweaver@umich.edu>
2024-07-09 19:47:47 -0400

    Tank drive running

--- cargo_beep/joystick_controller.py
+++ cargo_beep/joystick_controller.py
@@ -8,7 +8,8 @@ import signal
 
 settings = termios.tcgetattr(sys.stdin)
 
-MAX_VELOCITY = 3
+MAX_VELOCITY = 3.0
+TRIGGER_SCALE = .5
 
 # def getKey():
 # 	tty.setraw(sys.stdin.fileno())
@@ -17,16 +18,23 @@ MAX_VELOCITY = 3
 # 	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 # 	return key
 
-def velocity_cap (velocity):
-     return min(MAX_VELOCITY, max(velocity, -MAX_VELOCITY))
+def velocity_cap (velocity = Float32):
+     if velocity > MAX_VELOCITY:
+         return MAX_VELOCITY
+     return velocity
+
+    #  return min(MAX_VELOCITY, max(velocity, -MAX_VELOCITY))
 
 class JoystickControllerNode(Node):
     def __init__ (self):
         super().__init__("joystick_control")
         
-        self.left_velocity = 0
-        self.right_velocity = 0
+        self.left_velocity = 0.0
+        self.right_velocity = 0.0
         self.joystick = Joy()
+        self.left_trigger = 0.0
+        self.right_trigger = 0.0
+        
 
         signal.signal(signal.SIGINT, self.shutdown_cb)
         
@@ -59,40 +67,25 @@ class JoystickControllerNode(Node):
 
     def joystick_cb(self, msg):
         self.joystick = msg
-        
+        self.left_trigger = (-1 * self.joystick.axes[3] + 1) * TRIGGER_SCALE
+        self.right_trigger = (-1 * self.joystick.axes[4] + 1) * TRIGGER_SCALE     
+        print(f"Left: {self.left_trigger}", f"Right {self.right_trigger}")
     
     def timer_cb (self):
-
-        print(f"Square: {self.joystick.buttons}")
-        print(f"Cross {self.joystick.axes}")
-        # print(f"Circle: {self.joystick.buttons[2]}")
-        # print(f"Triangle: {self.joystick.buttons[3]}")
-        # key = getKey()
-        # if (key == 'w'):
-        #     self.left_velocity+=.05
-        #     self.right_velocity+=.05
-        # if (key == 's'):
-        #     self.left_velocity-=.05
-        #     self.right_velocity-=.05
-        # if (key == 'a'):
-        #     self.left_velocity+=.05
-        #     self.right_velocity-=.05
-        # if (key == 'd'):
-        #     self.left_velocity-=.05
-        #     self.right_velocity+=.05
-        # if (key == chr(27)):
-        #     self.shutdown_cb()
-
-        # print(f"left velocity: {self.left_velocity}")
-        # print(f"right velocity: {self.right_velocity}\n\n")
+       
+        self.left_velocity = self.left_trigger
+        self.right_velocity = self.right_trigger
+     
+        print(f"left velocity: {self.left_velocity}")
+        print(f"right velocity: {self.right_velocity}\n\n")
         
-        # msg0 = Float32()
-        # msg0.data = velocity_cap(self.right_velocity)
-        #self.velocity0_pub.publish(msg0)
+        msg0 = Float32()
+        msg0.data = velocity_cap(self.right_velocity)
+        self.velocity0_pub.publish(msg0)
 
-        # msg1 = Float32()
-        # msg1.data = velocity_cap(-self.left_velocity)
-        #self.velocity1_pub.publish(msg1)
+        msg1 = Float32()
+        msg1.data = velocity_cap(-self.left_velocity)
+        self.velocity1_pub.publish(msg1)
         
 
     
